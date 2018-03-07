24786d38ea5811805bfd98827034b7018841dc98
darrell <darrell.murray@gmail.com>
2018-03-07 09:19:13 -0500

    framework

---
+++ ClosedCapt2/ClosedCapt2/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClosedCapt2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IFaceServiceClient faceServiceClient = new FaceServiceClient("41fa2860658b4bc3b7c467f58f2a6cdf", "https://eastus.api.cognitive.microsoft.com/face/v1.0");
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public void Train()
+        {
+            var personGroupId = "1";
+            var personGroupName = "Darrell";
+            var filePath = "~/images/Darrell/";
+            var task = TrainFacialRecognizitionAsync(personGroupId, personGroupName, filePath);
+            task.Start();
+
+        }
+
+
+        public async Task TrainFacialRecognizitionAsync(string personGroupId, string personGroupName, string filePath)
+        {
+            CreatePersonResult person = await faceServiceClient.CreatePersonAsync(personGroupId, personGroupName);
+            foreach (string imagePath in Directory.GetFiles(filePath))
+            {
+                using (Stream s = System.IO.File.OpenRead(imagePath))
+                {
+                    await faceServiceClient.AddPersonFaceAsync(personGroupId, person.PersonId, s);
+                }
+            }
+        }
+    }
+}
